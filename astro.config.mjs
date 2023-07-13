3ff3e2bbac6e053c1951944c564fc390c501dffc
Augusto Pieper <augustoopieper@gmail.com>
2023-07-13 16:32:04 -0300

    added astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config'
-
 import sitemap from '@astrojs/sitemap'
+import compress from 'astro-compress'
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [sitemap()],
+	integrations: [sitemap(), compress()],
 	markdown: {
 		shikiConfig: {
 			theme: 'rose-pine',
