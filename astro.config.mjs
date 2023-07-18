128f1cc401c0e2d149cb948f688e5b834a643ce4
Augusto Pieper <augustoopieper@gmail.com>
2023-07-18 16:23:47 -0300

    fixed metatag banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap'
 import compress from 'astro-compress'
 
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://me.hatejs.dev',
 	integrations: [sitemap(), compress()],
 	markdown: {
 		shikiConfig: {
